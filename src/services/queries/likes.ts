bdeab71accbd06fd86d1823649508ed92d513944
Rodrigo Monte <rodrigo.monte86@gmail.com>
2024-05-26 16:34:32 -0400

    All changes up to and including Section 10

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,33 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemsKey, userLikesKey } from '$services/keys';
+import { getItems } from './items';
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
+    const ids = await client.sMembers(userLikesKey(userId));
+    return getItems(ids);
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+    if(inserted) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+
+    if(removed) {
+        return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+    return getItems(ids);
+};
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,45 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { usersKey, usernamesUniqueKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+	return deserialize(id, user);
+};
+
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const id = genId();
+
+	// Check if username already in the set of usernames
+	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+
+	// If so, throw an error
+	if(exists) {
+		throw new Error('Username is taken');
+	}
+
+	// Otherwise, create user and add to the set
+	await client.hSet(usersKey(id), serialize(attrs));
+	await client.sAdd(usernamesUniqueKey(), attrs.username);
+
+	return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
+		username: user.username,
+		password: user.password
+	};
+};
